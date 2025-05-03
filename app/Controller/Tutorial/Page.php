f470c3357b0c16d6affaa76e5e45d66b23c99910
fernando <fernandofernandes2280@gmail.com>
2025-05-03 15:12:04 -0300

    altera msg de erro

--- app/Controller/Tutorial/Page.php
+++ app/Controller/Tutorial/Page.php
@@ -20,163 +20,192 @@ class Page{
 	        'label' => 'Antes de Começar',
 	        'link' => URL.'/antesdecomecar',
 	        'classe' => '',
-	        'icon' => 'noise_control_off'
+	        'icon' => 'noise_control_off',
+			'style' => 'color: blue'
 	    ],
 	    'introducaoabancodedados' =>[
 	        'label' => 'Introdução a Banco de Dados',
 	        'link' => URL.'/introducaoabancodedados',
 	        'classe' => '',
-	        'icon' => 'noise_control_off'
+	        'icon' => 'noise_control_off',
+			'style' => 'color: blue'
 	    ],
 	    'oqueesql' =>[
 	        'label' => 'O que é SQL',
 	        'link' => URL.'/oqueesql',
             'classe' => '',
-	        'icon' => 'noise_control_off'
+	        'icon' => 'noise_control_off',
+			'style' => 'color: blue'
 	    ],
 	    'comandosbasicos' =>[
 	        'label' => 'Comandos Básicos',
 	        'link' => '',
 	        'classe' => 'disabled',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'noise_control_off',
+			'style' => 'color: blue'
 	    ],
 	    'createdatabase' =>[
 	        'label' => 'Create Database',
 	        'link' => URL.'/createdatabase',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'showdatabases' =>[
 	        'label' => 'Show Databases',
 	        'link' => URL.'/showdatabases',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'usedatabase' =>[
 	        'label' => 'Use Database',
 	        'link' => URL.'/usedatabase',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'createtable' =>[
 	        'label' => 'Create Table',
 	        'link' => URL.'/createtable',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black',
+			'style' => 'color: black'
 	    ],
 	    'showtables' =>[
 	        'label' => 'Show Tables',
 	        'link' => URL.'/showtables',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'showcolumns' =>[
 	        'label' => 'Show Columns',
 	        'link' => URL.'/showcolumns',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'altertable' =>[
 	        'label' => 'Alter Table',
 	        'link' => URL.'/altertable',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'restricoes' =>[
 	        'label' => 'Restrições (Constraints)',
 	        'link' => URL.'/restricoes',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'insert' =>[
 	        'label' => 'Insert',
 	        'link' => URL.'/insert',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'select' =>[
 	        'label' => 'Select',
 	        'link' => URL.'/select',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black',
+			
 	    ],
 	    'orderby' =>[
 	        'label' => 'Order By',
 	        'link' => URL.'/orderby',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'update' =>[
 	        'label' => 'Update',
 	        'link' => URL.'/update',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'delete' =>[
 	        'label' => 'Delete',
 	        'link' => URL.'/delete',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'droptable' =>[
 	        'label' => 'Drop Table',
 	        'link' => URL.'/droptable',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'truncatetable' =>[
 	        'label' => 'Truncate Table',
 	        'link' => URL.'/truncatetable',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'dropdatabase' =>[
 	        'label' => 'Drop Database',
 	        'link' => URL.'/dropdatabase',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'play_arrow'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'comandosavancados' =>[
 	        'label' => 'Comandos Avançados',
 	        'link' => '',
 	        'classe' => 'disabled',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'noise_control_off',
+			'style' => 'color: blue'
 	    ],
 	    'join' =>[
 	        'label' => 'Join',
 	        'link' => URL.'/join',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'innerjoin' =>[
 	        'label' => 'Inner Join',
 	        'link' => URL.'/innerjoin',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'leftjoin' =>[
 	        'label' => 'Left Join',
 	        'link' => URL.'/leftjoin',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'rightjoin' =>[
 	        'label' => 'Right Join',
 	        'link' => URL.'/rightjoin',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'union' =>[
 	        'label' => 'Union',
 	        'link' => URL.'/union',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ],
 	    'referencias' =>[
 	        'label' => 'Referências',
 	        'link' => URL.'/referencias',
 	        'classe' => 'nav-link-subitem',
-	        'icon' => 'arrow_drop_down'
+	        'icon' => 'play_arrow',
+			'style' => 'color: black'
 	    ]
 	];
 	
@@ -193,6 +222,8 @@ class Page{
 	            'link' => $module['link'],
 	            'classe' => $module['classe'],
 	            'icon' => $module['icon'],
+	            'style' => $module['style'],
+
 	            //'current' => $hash == $currentModule ? 'text-danger' : ''
 	            'current' => $hash == $currentModule ? 'active"  style="font-weight:bold;' : ''
 	            
